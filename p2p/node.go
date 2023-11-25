0bf42ff9c135c8e3f8ca2e55fe7be2bed7254036
Kamal Ahmad <kamalahmad22@pm.me>
2023-11-25 09:19:37 +0500

    Initial synchronization work

--- p2p/node.go
+++ p2p/node.go
@@ -5,38 +5,83 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"net/rpc"
+	"sort"
 	"sync"
+	"time"
 )
 
 // TODO: add mutexes for both node access itself and also block_chain
 type Node struct {
-	lock sync.RWMutex
+	lock    sync.RWMutex
 	address string
+	// The port the node is listening on
+	port        uint16
 	block_chain *blockchain.BlockChain
-	peers map[string]*rpc.Client
+	peers       map[string]*rpc.Client
+}
+
+func NewNode(server net.Listener, block_chain *blockchain.BlockChain) *Node {
+	var port uint16
+	switch addr := server.Addr().(type) {
+	case *net.UDPAddr:
+		port = addr.AddrPort().Port()
+	case *net.TCPAddr:
+		port = addr.AddrPort().Port()
+	}
+	node := Node{sync.RWMutex{}, server.Addr().String(), port, block_chain, make(map[string]*rpc.Client, 0)}
+	log.Printf("Listening on %v\n", server.Addr())
+	// Start the RPC responder service
+	go func(server net.Listener) {
+		for {
+			conn, err := server.Accept()
+			if err != nil {
+				log.Println("Error accepting connection on server", err)
+			}
+			log.Println("New connection from", conn.RemoteAddr().String())
+			responder := NewRPCResponder(&node, conn)
+			server := rpc.NewServer()
+			if err := server.Register(&responder); err != nil {
+				log.Println("Error registering RPC methods", err)
+				conn.Close()
+			}
+			go server.ServeConn(conn)
+		}
+	}(server)
+	return &node
 }
 
 func (node *Node) Connect(address string) error {
-	client, err := rpc.DialHTTP("tcp", address)
-	if err != nil {
-		return err
+	_, already_connected := node.peers[address]
+	if !already_connected {
+		log.Println("Node connecting to ", address)
+		client, err := rpc.Dial("tcp", address)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		node.lock.Lock()
+		defer node.lock.Unlock()
+		node.peers[address] = client
+		var reply string
+		if err := client.Call("RPCResponder.Connect", node.port, &reply); err != nil {
+			log.Println("Error calling RPCResponder.Connect", err)
+		}
+	} else {
+		log.Println("Already connected to", address)
 	}
-	node.peers[address] = client
-	var reply string
-	// We don't care about errors here, just telling the node we're connecting to that they can also connect to us
-	// TODO: this probably won't actually work, because listener.Addr() gives 0.0.0.0 or something similar whereas we need our actual IP
-	client.Call("Node.ConnectTo", node.address, &reply)
+
 	return nil
 }
 
 // This is *not* an RPC call. It's simply used for debugging. It prints all of the peers of nodes we're connected to
 func (node *Node) PrintConnectedPeers() {
+	node.lock.RLock()
+	defer node.lock.RUnlock()
 	for addr, peer := range node.peers {
-		log.Println("Requesting peers of ", addr)
+		log.Println("Requesting peers of", addr)
 		reply := make([]string, 0)
-		err := peer.Call("Node.GetPeers", "", &reply)
+		err := peer.Call("RPCResponder.GetPeers", "", &reply)
 		if err != nil {
 			log.Println(err)
 			continue
@@ -47,11 +92,101 @@ func (node *Node) PrintConnectedPeers() {
 	}
 }
 
-func NewNode(server net.Listener, block_chain *blockchain.BlockChain) *Node {
-	node := Node{sync.RWMutex{}, server.Addr().String(), block_chain, make(map[string]*rpc.Client, 0)}
-	rpc.Register(&node)
-	rpc.HandleHTTP()
-	log.Printf("Listening on %v\n", server.Addr())
-	go http.Serve(server, nil)
-	return &node
+// Synchronizes node with longest chain on network.
+func (node *Node) Sync() {
+	log.Println("Node.Sync called")
+	// Request headers
+
+	// Channel used to receive list of block headers
+
+	type channel_value struct {
+		peer_addr string
+		blocks    []blockchain.Block
+	}
+	channel := make(chan channel_value, len(node.peers))
+
+	i := 0
+	for addr, peer := range node.peers {
+		args := struct {
+			Start int
+			End   int
+			bool
+		}{-1, -1, true}
+		go func(channel chan<- channel_value) {
+			headers := make([]blockchain.Block, 0)
+			if err := peer.Call("RPCResponder.GetBlocks", args, &headers); err != nil {
+				log.Println("Error calling RPCResponder.GetBlocks on peer", addr, err)
+				node.lock.RUnlock()
+				return
+			}
+			channel <- channel_value{addr, headers}
+		}(channel)
+		i++
+	}
+	// Sorted by max length
+	var peer_headers [][]blockchain.Block
+outer:
+	for {
+		select {
+		case headers := <-channel:
+			all := true
+			for i := range headers.blocks {
+				if !blockchain.VerifyBlockHeader(headers.blocks, uint32(i)) {
+					log.Println("Verification of headers from ", headers.peer_addr, "failed")
+					all = false
+					break
+				}
+			}
+			if !all {
+				continue
+			}
+			peer_headers = append(peer_headers, headers.blocks)
+		case <-time.After(time.Second * 20):
+			fmt.Println("Timeout reached")
+			break outer
+		}
+	}
+	sort.Slice(peer_headers, func(i, j int) bool {
+		return len(peer_headers[i]) > len(peer_headers[j])
+	})
+	if len(peer_headers) == 0 {
+		log.Println("All peer headers are invalid or of 0 length. Node is synchronized")
+		return
+	}
+	log.Println("Max block headers received", len(peer_headers[0]))
+
+	node.lock.RLock()
+	if len(peer_headers[0]) <= len(node.block_chain.Blocks) {
+		log.Println("Node is already synchronized, network #headers:", len(peer_headers[0]), "we have:", len(node.block_chain.Blocks))
+	}
+	node.lock.RUnlock()
+
+	node.lock.RLock()
+	// Request full blocks from all peers
+	blocks := make([]blockchain.Block, 0)
+	for _, header := range peer_headers[0] {
+		log.Printf("Requesting full block %x\n", header.Header.BlockHash())
+		args := struct{BlockHash [32]byte; Count uint}{[32]byte(header.Header.BlockHash()), 1}
+		channel := make(chan blockchain.Block)
+		// This isn't as efficient as it could be, we send a request to all peers for a certain block and get the first one that responds
+		for addr, peer := range node.peers {
+			go func(peer *rpc.Client, addr string, channel chan <- blockchain.Block) {
+				reply := make([]blockchain.Block, 1)
+				if err := peer.Call("RPCResponder.GetBlocksByHash", args, &reply); err != nil {
+					log.Println("Error getting block from peer", addr, err)
+				}
+				if len(reply) != 1 {
+					log.Println("Peer", addr, "did not send enough blocks")
+				} else {
+					channel <- reply[0]
+				}
+			}(peer, addr, channel)
+		}
+		block := <- channel
+		blocks = append(blocks, block)
+	}
+	node.lock.RUnlock()
+	log.Println("Sync completed, attempting to build longest chain")
+	node.lock.Lock()
+	log.Println("AttemptOrphan result", node.block_chain.AttemptOrphan(blocks))
 }
--- p2p/rpc.go
+++ p2p/rpc.go
@@ -6,45 +6,104 @@ import (
 	"errors"
 	"log"
 	"net"
+	"strconv"
 )
 
 type RPCResponder struct {
-		node *Node
-		conn net.Conn
+	Node *Node
+	Conn net.Conn
 }
 
-func (rpc *RPCResponder) GetBlocks(args *struct{start int; end int}, reply *[]blockchain.Block) error {
-	node := rpc.node
-	rpc.node.lock.RLock()
-	defer rpc.node.lock.RUnlock()
-	if args.start > args.end {
+func NewRPCResponder(node *Node, conn net.Conn) RPCResponder {
+	return RPCResponder{node, conn}
+}
+
+// Request blocks[start..end]. If end == start == -1 then return all blocks
+func (rpc *RPCResponder) GetBlocks(args *struct {
+	Start int
+	End   int
+	HeadersOnly bool
+}, reply *[]blockchain.Block) error {
+	node := rpc.Node
+	rpc.Node.lock.RLock()
+	defer rpc.Node.lock.RUnlock()
+
+	if args.Start > args.End {
 		return errors.New("start > end")
 	}
-	if args.start >= len(node.block_chain.Blocks) {
+	if args.Start >= len(node.block_chain.Blocks) {
 		return errors.New("start > total number of blocks")
 	}
-	blocks := make([]blockchain.Block, 0, args.end - args.start)
-	end := len(node.block_chain.Blocks)
-	if args.end < end {
-		end = end
+	start, end := 0, 0
+	if args.Start == args.End && args.End == -1 {
+		start = 0
+		end = len(node.block_chain.Blocks)
+	} else if args.End <= len(node.block_chain.Blocks) {
+		end = args.End
+	} else {
+		return errors.New("end > total Number of blocks")
 	}
-	for _, block := range node.block_chain.Blocks[args.start : args.end] {
+	log.Println("Start, end:", start,end)
+	blocks := make([]blockchain.Block, 0, args.End-args.Start)
+
+	for _, block := range node.block_chain.Blocks[start:end] {
 		blocks = append(blocks, block)
+		if args.HeadersOnly {
+			blocks[len(blocks)-1].Transactions = make([]blockchain.Transaction, 0)
+		}
 	}
 	*reply = blocks
 	return nil
 }
 
+// Return count blocks after block_hash
+func (rpc *RPCResponder) GetBlocksByHash(args struct {BlockHash [32]byte; Count uint}, reply *[]blockchain.Block) error {
+	node := rpc.Node
+	rpc.Node.lock.RLock()
+	defer rpc.Node.lock.RUnlock()
+
+	if idx := node.block_chain.SearchBlockByHash(args.BlockHash); idx == nil {
+		return errors.New("Block Not Found")
+	} else {
+		*reply = make([]blockchain.Block, 0, args.Count)
+		end := *idx + int(args.Count)
+		if end >= len(node.block_chain.Blocks) {
+			end = len(node.block_chain.Blocks)
+		}
+		for i := *idx; i < end; i++ {
+			*reply = append(*reply, node.block_chain.Blocks[i])
+		}
+		return nil
+	}
+}
+
 
 // This RPC call returns all the peers the RPC server is connected to
 func (rpc *RPCResponder) GetPeers(args string, reply *[]string) error {
-	node := rpc.node
+	node := rpc.Node
 	node.lock.RLock()
 	defer node.lock.RUnlock()
-	log.Println("RPCResponder.PrintPeers called")
+	log.Println("RPCResponder.GetPeers called")
 	*reply = make([]string, 0)
-	for addr, _ := range node.peers {
+	for addr := range node.peers {
 		*reply = append(*reply, addr)
 	}
+	return nil
+}
+
+// Signals to the RPC server that the client is accepting inbound connections on their node on port *args*
+func (rpc *RPCResponder) Connect(port uint16, reply *string) error {
+	log.Println("RPCResponder.Connect called")
+	var conn_addr string
+	switch addr := rpc.Conn.RemoteAddr().(type) {
+	case *net.UDPAddr:
+		conn_addr = addr.IP.String()
+	case *net.TCPAddr:
+		conn_addr = addr.IP.String()
+	}
+	conn_addr += ":" + strconv.Itoa(int(port))
+	log.Println("Attempting connection to", conn_addr)
+	go rpc.Node.Connect(conn_addr)
+	*reply = ""
 	return nil
 }
